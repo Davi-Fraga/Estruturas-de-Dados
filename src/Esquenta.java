d1fb2c2b1b4146c3d6bf5e56e9dd959111fb1784
Davi Fraga <fragadavi30@gmail.com>
2026-02-05 20:14:55 -0300

    corrigindo

--- src/Esquenta.java
+++ src/Esquenta.java
@@ -1,31 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Esquenta {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
+        //Inteiros
         byte b = 121;
-        System.out.println("byte"+ b);
-
-        int i = 7;
-        System.out.println("Int"+ i);
-
-        double d = 2.99;
-        System.out.println("Double" + d);
+        System.out.println("byte: "+ b);
 
         short s = - 30000;
-        System.out.println("Short" + s);
+        System.out.println("Short: " + s);
+
+        int i = 7;
+        System.out.println("Int: "+ i);
+
 
         long l = 500000000;
-        System.out.println("Long" + l);
+        System.out.println("Long: " + l);
 
+        //Pontos Flutuantes
         float f = 400;
-        System.out.println(" Float" + f);
+        System.out.println("Float: " + f);
 
+        double d = 2.99;
+        System.out.println("Double: " + d);
+
+
+        //Caractere
         char c = 'y';
-        System.out.println("Char" + c);
+        System.out.println("Char: " + c);
 
+        //Lógico
         boolean bo = true;
-        System.out.println("Boolean" + bo);
+        System.out.println("Boolean: " + bo);
     }
 }
