a1a41b8a83ae0ddfc6542d582cfbafc097d0ddc8
Davi Fraga <fragadavi30@gmail.com>
2026-02-05 19:57:07 -0300

    Meu primeiro commit

---
+++ src/Esquenta.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Esquenta {
+    public static void main(String[] args) {
+
+
+        byte b = 121;
+        System.out.println("byte"+ b);
+
+        int i = 7;
+        System.out.println("Int"+ i);
+
+        double d = 2.99;
+        System.out.println("Double" + d);
+
+        short s = - 30000;
+        System.out.println("Short" + s);
+
+        long l = 500000000;
+        System.out.println("Long" + l);
+
+        float f = 400;
+        System.out.println(" Float" + f);
+
+        char c = 'y';
+        System.out.println("Char" + c);
+
+        boolean bo = true;
+        System.out.println("Boolean" + bo);
+    }
+}
